cf7d8f98327c5aaae60df779a76aa79280b27037
YaroslavZvonov <sa50_ia.a.zvonov@mpt.ru>
2022-04-22 10:06:30 +0300

    feat: xto ya

--- laba15/script.js
+++ laba15/script.js
@@ -1,13 +1,13 @@
-anime({
-    keyframes: [
-      {translateY: -40},
-      {translateX: 250},
-      {translateY: 40},
-      {translateX: 0},
-      {translateY: 0}
-    ],
-    duration: 1000,
-    easing: 'easeOutElastic(1, .8)',
-    loop: true
-    targets: '.animation-keyframes-demo .el',
-  });
+function anime(){
+  var block = document.getElementById("kubik");
+  var kub = 0;
+  var id = setInterval(frame, 10);
+  function frame(){
+    if (kub == 425){
+      setTimeout(function() {anime()}, 0);
+    } else{
+      kub++;
+      block.style.left= kub + 'px';
+    }
+  }
+}
